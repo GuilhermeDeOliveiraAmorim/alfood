23f7d2f71e06541bf045dfddb17210e7ffdf0af7
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-05-23 06:02:32 -0300

    feat: verMais restaurants working

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -8,45 +8,46 @@ import Restaurante from "./Restaurante";
 const ListaRestaurantes = () => {
 
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+
     const [proximaPagina, setProximaPagina] = useState('');
+
     const verMais = () => {
-        axios.get<IPaginacao<IRestaurante>>(proximaPagina)
-        .then(
-            resposta => {
+        axios
+            .get<IPaginacao<IRestaurante>>(proximaPagina)
+            .then((resposta) => {
                 setRestaurantes([...restaurantes, ...resposta.data.results])
                 setProximaPagina(resposta.data.next)
-            }
-        )
-        .catch(
-            erro => {
-                console.log(erro)
-            }
-        )
-    }
-  
+            })
+            .catch((erro) => {
+                console.log(erro);
+            });
+    };
+
     useEffect(() => {
-        axios.get<IPaginacao<IRestaurante>>('http://0.0.0.0:8000/api/v1/restaurantes/')
-        .then(
-            resposta => {
+        axios
+            .get<IPaginacao<IRestaurante>>(
+                "http://0.0.0.0:8000/api/v1/restaurantes/"
+            )
+            .then((resposta) => {
                 setRestaurantes(resposta.data.results)
-            }
-        )
-        .catch(
-            erro => {
-                console.log(erro)
-            }
-        )
+                setProximaPagina(resposta.data.next)
+            })
+            .catch((erro) => {
+                console.log(erro);
+            });
     }, []);
 
     return (
         <section className={style.ListaRestaurantes}>
-            <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-            {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-            {proximaPagina && <button onClick={verMais}>
-                ver mais
-            </button>}
+            <h1>
+                Os restaurantes mais <em>bacanas</em>!
+            </h1>
+            {restaurantes?.map((item) => (
+                <Restaurante restaurante={item} key={item.id} />
+            ))}
+            <button onClick={verMais}>ver mais</button>
         </section>
-    )
-}
+    );
+};
 
 export default ListaRestaurantes;
