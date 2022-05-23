871956a48a3e4cf446f1cf711ee8c04a2a41ec0a
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-05-23 05:50:50 -0300

    feat: add verMais button for pagination

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { IPaginacao } from "../../interfaces/IPaginacao";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
@@ -7,9 +8,24 @@ import Restaurante from "./Restaurante";
 const ListaRestaurantes = () => {
 
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+    const [proximaPagina, setProximaPagina] = useState('');
+    const verMais = () => {
+        axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+        .then(
+            resposta => {
+                setRestaurantes([...restaurantes, ...resposta.data.results])
+                setProximaPagina(resposta.data.next)
+            }
+        )
+        .catch(
+            erro => {
+                console.log(erro)
+            }
+        )
+    }
   
     useEffect(() => {
-        axios.get('http://0.0.0.0:8000/api/v1/restaurantes/')
+        axios.get<IPaginacao<IRestaurante>>('http://0.0.0.0:8000/api/v1/restaurantes/')
         .then(
             resposta => {
                 setRestaurantes(resposta.data.results)
@@ -24,14 +40,13 @@ const ListaRestaurantes = () => {
 
     return (
         <section className={style.ListaRestaurantes}>
-            <h1>
-                Os restaurantes mais <em>bacanas</em>!
-            </h1>
-            {restaurantes?.map((item) => (
-                <Restaurante restaurante={item} key={item.id} />
-            ))}
+            <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+            {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+            {proximaPagina && <button onClick={verMais}>
+                ver mais
+            </button>}
         </section>
-    );
-};
+    )
+}
 
 export default ListaRestaurantes;
