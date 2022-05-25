f09bb54d98afd0ef06d0d17081af77da3d2bc751
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-05-25 06:15:51 -0300

    feat: add nav bar to admin layout with Outlet

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -13,7 +13,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import http from "../Http";
-import { Link as LinkDom } from 'react-router-dom';
+import { Link as LinkDom } from "react-router-dom";
 
 const FormularioRestaurante = () => {
     const parametros = useParams();
@@ -53,64 +53,34 @@ const FormularioRestaurante = () => {
 
     return (
         <>
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar>
-                        <Typography variant="h6">Admnistração</Typography>
-                        <Box sx={{ display: "flex", flexGrow: 1 }}>
-                            <Link component={LinkDom} to="/admin/restaurantes">
-                                <Button sx={{ my: 2, color: "white" }}>
-                                    Restaurantes
-                                </Button>
-                            </Link>
-                            <Link component={LinkDom} to="/admin/restaurantes/novo">
-                                <Button sx={{ my: 2, color: "white" }}>
-                                    Novo Restaurante
-                                </Button>
-                            </Link>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-
-            <Box>
-                <Container maxWidth="lg" sx={{ mt: 1 }}>
-                    <Paper sx={{ p: 2 }}>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                flexDirection: "column",
-                                alignItems: "center",
-                            }}
-                        >
-                            <Typography component="h1" variant="h6">
-                                Inserir Novo Restaurante
-                            </Typography>
-                            <Box component="form" onSubmit={aoSubmeterForm}>
-                                <TextField
-                                    value={nomeRestaurante}
-                                    onChange={(evento) =>
-                                        setNomeRestaurante(evento.target.value)
-                                    }
-                                    id="standard-basic"
-                                    label="Nome do Restaurante"
-                                    variant="standard"
-                                    fullWidth
-                                    required
-                                />
-                                <br />
-                                <br />
-                                <Button
-                                    type="submit"
-                                    variant="outlined"
-                                    fullWidth
-                                >
-                                    Salvar
-                                </Button>
-                            </Box>
-                        </Box>
-                    </Paper>
-                </Container>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography component="h1" variant="h6">
+                    Inserir Novo Restaurante
+                </Typography>
+                <Box component="form" onSubmit={aoSubmeterForm}>
+                    <TextField
+                        value={nomeRestaurante}
+                        onChange={(evento) =>
+                            setNomeRestaurante(evento.target.value)
+                        }
+                        id="standard-basic"
+                        label="Nome do Restaurante"
+                        variant="standard"
+                        fullWidth
+                        required
+                    />
+                    <br />
+                    <br />
+                    <Button type="submit" variant="outlined" fullWidth>
+                        Salvar
+                    </Button>
+                </Box>
             </Box>
         </>
     );
