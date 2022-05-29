5de380dfc7ba20072bf7e395f28c759d0b35117b
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-05-29 16:37:39 -0300

    feat: add restaurants in add prato

--- src/paginas/Administracao/Pratos/FormularioPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -19,17 +19,24 @@ import IPrato from "../../../interfaces/IPrato";
 import http from "../Http";
 import { Link as LinkDom } from "react-router-dom";
 import ITag from "../../../interfaces/ITag";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 const FormularioPrato = () => {
 
     const [nomePrato, setNomePrato] = useState('');
     const [descricaoPrato, setDescricaoPrato] = useState('');
+
     const [tags, setTags] = useState<ITag[]>([]);
     const [tag, setTag] = useState('');
+
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+    const [restaurante, setRestaurante] = useState('');
 
     useEffect(() => {
         http.get<{tags: ITag[]}>('tags/')
             .then(resposta => setTags(resposta.data.tags))
+        http.get<IRestaurante[]>('restaurantes/')
+            .then(resposta => setRestaurantes(resposta.data))
     }, [])
 
     const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
@@ -48,6 +55,7 @@ const FormularioPrato = () => {
                 <Typography component="h1" variant="h6">
                     Inserir Novo Prato
                 </Typography>
+
                 <Box component="form" onSubmit={aoSubmeterForm}>
                     <TextField
                         value={nomePrato}
@@ -61,6 +69,7 @@ const FormularioPrato = () => {
                         required
                         margin="dense"
                     />
+
                     <TextField
                         value={descricaoPrato}
                         onChange={(evento) =>
@@ -73,6 +82,7 @@ const FormularioPrato = () => {
                         required
                         margin="dense"
                     />
+
                     <FormControl margin="dense" fullWidth>
                         <InputLabel id="select-tag">Tag</InputLabel>
                         <Select labelId="select-tag" value={tag} onChange={evento => setTag(evento.target.value)}>
@@ -81,6 +91,16 @@ const FormularioPrato = () => {
                             </MenuItem>)}
                         </Select>
                     </FormControl>
+
+                    <FormControl margin="dense" fullWidth>
+                        <InputLabel id="select-restaurante">Restaurante</InputLabel>
+                        <Select labelId="select-restaurante" value={restaurante} onChange={evento => setRestaurante(evento.target.value)}>
+                            {restaurantes.map(restaurante => <MenuItem key={restaurante.id} value={restaurante.id}>
+                                {restaurante.nome}
+                            </MenuItem>)}
+                        </Select>
+                    </FormControl>
+
                     <Button type="submit" variant="outlined" fullWidth>
                         Adicionar
                     </Button>
